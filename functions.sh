ac11c0e8c1bf40e82f0f6a4780d928962652cc7a
ddolcimascolo <ddolcimascolo@linagora.com>
2017-06-08 17:22:33 +0200

    Finalized jv_pg_op_createMeeting function.

--- functions.sh
+++ functions.sh
@@ -1,8 +1,20 @@
 jv_pg_op_nextMeeting() {
-  curl -sq -u"${jv_pg_op_username}:${jv_pg_op_password}" -o /tmp/jv_pg_op_curl -H"Accept-Language: fr" ${jv_pg_op_url}/calendar/api/events/next/text && say "`cat /tmp/jv_pg_op_curl`"
+    curl    -sq \
+            -u"${jv_pg_op_username}:${jv_pg_op_password}" \
+            -H"Accept-Language: fr" \
+            ${jv_pg_op_url}/calendar/api/events/next/text
 }
 
 jv_pg_op_createMeeting() {
-  curl -sq -XPOST -u"${jv_pg_op_username}:${jv_pg_op_password}" -o /tmp/jv_pg_op_curl -H"Accept-Language: fr" ${jv_pg_op_url}/calendar/api/events
+    curl    -sq \
+            -u"${jv_pg_op_username}:${jv_pg_op_password}" \
+            -XPOST \
+            -H"Accept-Language: fr" \
+            -H"Content-Type: application/json" \
+            -d"{
+                \"summary\": \"${jv_pg_op_createMeeting_summary}\",
+                \"location\": \"${jv_pg_op_createMeeting_location}\",
+                \"when\": \"${jv_pg_op_createMeeting_when}\"
+            }" \
+            ${jv_pg_op_url}/calendar/api/events
 }
-
